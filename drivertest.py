769170784d05081911134a112f3342f38be1c2f3
Shobhit Gupta <shobhitg315@gmail.com>
2024-05-05 19:31:24 -0400

    testing with driver

--- drivertest.py
+++ drivertest.py
@@ -1,23 +1,33 @@
+import streamlit as st
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
-import streamlit as st
-@st.cache_resource
-def get_driver():
-    return webdriver.Chrome(
-        service=Service(
-            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-        ),
-        options=options,
-    )
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+import time
+import pandas as pd
 
-options = Options()
-options.add_argument("--disable-gpu")
-options.add_argument("--headless")
+with webdriver.Chrome(options=Options(),
+                        service=Service()) as driver:
+        url = "https://www.pokernow.club/games/pglyWT62uDgl7WkaOmbX5dter?fbclid=IwAR0Uo3V8oSWC-utS30kfNYqEITPOWknlB5jRr_l6CGmNH0OGlr_lNO066xc"
+        try:
+            driver.get(url)
+            actions = ActionChains(driver)
+            actions.send_keys(Keys.CONTROL + 'l').perform()
+            time.sleep(1)
+            ledger_button = driver.find_element(By.CLASS_NAME, 'ledger-button')
+            ledger_button.click()
+            time.sleep(1)
+            # Get the page source after the table is loaded
+            page_source = driver.page_source
 
-driver = get_driver()
-driver.get("http://example.com")
-
-st.code(driver.page_source)
+            # Close the webdriver
+            driver.quit()
+            print(page_source)
+        except:
+            print("hefljka")
