29f3cdd560371f213a381eb7a191ded5bdbb289a
Shobhit Gupta <shobhitg315@gmail.com>
2024-05-19 13:26:34 -0400

    matching to main

--- pokernow.py
+++ pokernow.py
@@ -1,24 +1,31 @@
 import streamlit as st
+import time
+import pandas as pd
+import heapq
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
-from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService
-import time
-import pandas as pd
+
+from bs4 import BeautifulSoup
+
+
 
 def get_driver():
-    service = Service()
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     return webdriver.Chrome(options=options)
+
+def df_to_float(value):
+    try:
+        return float(value)
+    except (TypeError, ValueError):
+        return value
 
 def get_ledger(url):
     
@@ -59,8 +66,9 @@ def get_ledger(url):
                 row[0] = row[0].split("@")[0]
                 players.append(row)
     df = pd.DataFrame(players, columns=titles)
-    return df
-import heapq
+    df.rename(columns={'Net↓':'Net'}, inplace=True)
+    
+    return df.iloc[:-1], list(df.iloc[-1])
 
 def generate_payouts(player_net_tuples):
     # Convert net amounts to floats
@@ -82,32 +90,53 @@ def generate_payouts(player_net_tuples):
     payouts = []
     
     # Distribute positive net amounts to players with negative net amounts
-    for payer, amount in positive_net:
+    for receiver, amount in positive_net:
         while amount > 0 and negative_net:
             # Extract player with the smallest negative net amount
-            debt, receiver = heapq.heappop(negative_net)
+            debt, payer = heapq.heappop(negative_net)
             payment = min(amount, -debt)
-            payouts.append((receiver, payer, payment))  # Reverse payer and receiver
+            payouts.append((payer, receiver, payment))  # Reverse payer and receiver
             amount -= payment
             # If there is remaining debt, push it back to the heap
             if debt + payment < 0:
-                heapq.heappush(negative_net, (debt + payment, receiver))
-    
+                heapq.heappush(negative_net, (debt + payment, payer))
+                
+    #sort so the same name has the payouts together            
+    payouts.sort(key=lambda x: x[0])
     return payouts
 
-# URL of the Poker Now website
+st.set_page_config(page_title='PokerNow Payouts', page_icon=":spades:", layout='wide')
+
+st.title("Poker Now Payouts :spades:")
+st.write("This app can be used to generate the payouts for any poker game played on PokerNow. Just copy and paste the game link below")
+
+
 with st.form("URL"):
     url = st.text_input("Enter game URL")
     submitted = st.form_submit_button("Get Payouts")
 if url and submitted:
-    # Open the URL in the browser
-    df = get_ledger(url)
 
-    st.write("Ledger:")
-    st.dataframe(df)
-    player_net_tuples = list(df[['Player', 'Net↓']].to_records(index=False))[:-1]
-    st.write(player_net_tuples)
+    df, total = get_ledger(url)
+    
+    
+    
+    player_net_tuples = list(df[['Player', 'Net']].to_records(index=False))
+    
     payouts = generate_payouts(player_net_tuples)
-    st.write("PAYOUTS")
-    for payer, receiver, amount in payouts:
-        st.write(f"{payer.strip()} pays {receiver.strip()} {amount:.2f}")
+    cols = ["", "Buy-In", "Buy-Out", "Stack"]
+
+    # Create DataFrame from list and transpose it
+    total_df = pd.DataFrame([total[:-1]], columns=cols)
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.header("Ledger:")
+        st.dataframe(df, hide_index=True)
+        
+        st.dataframe(total_df, hide_index=True)
+
+    # Display payouts in the second column
+    with col2:
+        st.header("Payouts:")
+        for payer, receiver, amount in payouts:
+            st.write(f"{payer.strip()} pays {receiver.strip()} {amount:.2f}")
